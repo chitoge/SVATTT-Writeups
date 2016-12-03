33d3bcaa24fe31c3c9746c272a33566b5e096d55
Minh Tran <myoki.crystal@gmail.com>
2016-12-03 16:09:01 +0700

    fix

--- finals/forensics/solve.py
+++ finals/forensics/solve.py
@@ -1,11 +1,5 @@
 def parse(data):
-	#print ord(data), type(data)
-	if ord(data) > 64 and ord(data) < 71: 
-		return ord(data) - 55
-	elif int(data) > 0 and int(data) < 10:
-		return int(data)
-	else: 
-		return -1
+	return int(data,16)
 
 if __name__ == '__main__':
 	f = open('chal','rb')
@@ -22,7 +16,7 @@ if __name__ == '__main__':
 			break
 		cursor += 3
 		chunk = file[cursor:cursor+size]
-		cursor = cursor + size + 2
+		cursor += size + 2
 		res += chunk
 		#print size, chunk.encode('hex')
 	print res.encode('hex')
